b79e51871f048a81c43fa12b2c6a8d3346fea7a0
web-pers <enmds13@gmail.com>
2025-03-02 16:43:04 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll(".content");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    }, { threshold: 0.2 });
+
+    sections.forEach(section => observer.observe(section));
+});
